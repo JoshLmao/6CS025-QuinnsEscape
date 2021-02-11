4e6c7caee58fb0f1d021b2a1613e220c435420b8
Josh Shepherd <josh_lmao@live.co.uk>
2021-02-11 15:35:21 +0000

    Initialised project with Side Scroller template

---
+++ QuinnsEscape/Source/QuinnsEscape/QuinnsEscapeGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "QuinnsEscapeGameMode.h"
+#include "QuinnsEscapeCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AQuinnsEscapeGameMode::AQuinnsEscapeGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ QuinnsEscape/Source/QuinnsEscape/QuinnsEscapeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuinnsEscapeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AQuinnsEscapeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AQuinnsEscapeGameMode();
+};
+
+
+
