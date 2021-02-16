a6d70790049c229d4fbc622f89787d86114b26ba
Josh Shepherd <josh_lmao@live.co.uk>
2021-02-16 19:47:13 +0000

    Added deadzone camera

---
+++ QuinnsEscape/Source/QuinnsEscape/World/DeadzoneCamera.cpp
@@ -0,0 +1,106 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DeadzoneCamera.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ADeadzoneCamera::ADeadzoneCamera()
+{
+	YDeadzone = 500;
+	ZDeadzone = 200;
+	CameraFieldOfView = 80.0f;
+
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create camera component
+	m_camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	m_camera->SetFieldOfView(CameraFieldOfView);
+}
+
+// Called when the game starts or when spawned
+void ADeadzoneCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Set the main player camera to be this camera
+	APlayerController* pc = UGameplayStatics::GetPlayerController(this, 0);
+	pc->SetViewTarget(this, FViewTargetTransitionParams());
+
+	// Set actor to follow
+	ActorToFollow = pc->GetPawn();
+
+	// Set field of view on start
+	m_camera->SetFieldOfView(CameraFieldOfView);
+}
+
+// Called every frame
+void ADeadzoneCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Set camera to follow if given an actor
+	if (IsValid(ActorToFollow))
+	{
+		// Get Y and Z values
+		m_camY = GetCameraY();
+		m_camZ = GetCameraZ();
+
+		// Update actor's position to target location
+		FVector loc = FVector(XOffset, m_camY, m_camZ + ZOffset);
+		SetActorLocation(loc);
+	}
+}
+
+float ADeadzoneCamera::GetCameraY()
+{
+	FVector followLocation = ActorToFollow->GetActorLocation();
+	
+	// Check if actor to follow has moved left into deadzone
+	float minus = followLocation.Y - YDeadzone;
+	if (minus > GetActorLocation().Y)
+	{
+		return minus;
+	}
+
+	// Check if actor has moved right into deadzone
+	float add = followLocation.Y + YDeadzone;
+	if (add < GetActorLocation().Y)
+	{
+		return add;
+	}
+
+	// Return last cam X if player still within active zone
+	return m_camY;
+}
+
+float ADeadzoneCamera::GetCameraZ()
+{
+	FVector followLocation = ActorToFollow->GetActorLocation();
+
+	// Set to Z if a value is given
+	if (FixedZValue != 0)
+	{
+		return FixedZValue;
+	}
+
+	// Update to move down if within deadzone
+	float minus = followLocation.Z - ZDeadzone;
+	if (minus > GetActorLocation().Z)
+	{
+		return minus;
+	}
+
+	// Update to move up if in deadzone
+	float add = followLocation.Z + ZDeadzone;
+	if (add < GetActorLocation().Z)
+	{
+		return add;
+	}
+
+
+	return m_camZ;
+}
---
+++ QuinnsEscape/Source/QuinnsEscape/World/DeadzoneCamera.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DeadzoneCamera.generated.h"
+
+UCLASS()
+class QUINNSESCAPE_API ADeadzoneCamera : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADeadzoneCamera();
+
+	/*
+	*	VARIABLES
+	*/
+public:
+	UPROPERTY(EditAnywhere)
+	class AActor* ActorToFollow;
+
+	// Field of view of camera
+	UPROPERTY(EditAnywhere)
+	float CameraFieldOfView;
+
+	// Amount of offset between the character and the player
+	UPROPERTY(EditAnywhere)
+	float XOffset;
+	// Amount to add to offset the height
+	UPROPERTY(EditAnywhere)
+	float ZOffset;
+
+	// Amount of deadzone to use on the Y axis (Horizontal)
+	UPROPERTY(EditAnywhere)
+	float YDeadzone;
+	// Amount of deadzone to use on the Z axis (height)
+	UPROPERTY(EditAnywhere)
+	float ZDeadzone;
+
+	// Fixed height value to lock to instead of following the height
+	UPROPERTY(EditAnywhere)
+	float FixedZValue;
+
+private:
+	// Camera component of actor
+	class UCameraComponent* m_camera;
+
+	// Current Y position of camera
+	float m_camY;
+	// Current Z position of camera
+	float m_camZ;
+
+	/*
+	*	METHODS
+	*/
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	// Get the current camera Y positin if moved out of deadzone
+	float GetCameraY();
+	// Get the current camera Z position if moved out of deadzone
+	float GetCameraZ();
+};
